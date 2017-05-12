cc08fbf2242fcbee44fff2aca4b718aa696445d2
ymitev <yavor.mitev@securax.org>
2017-05-13 02:26:20 +0300

    Add header to the listview.

--- app/src/main/java/com/example/yavor/naxexmobile/MainActivity.java
+++ app/src/main/java/com/example/yavor/naxexmobile/MainActivity.java
@@ -4,10 +4,14 @@ import android.os.Bundle;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.AbsListView;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static android.content.res.Configuration.ORIENTATION_PORTRAIT;
 
 public class MainActivity extends AppCompatActivity
         implements LoaderManager.LoaderCallbacks<List<QuotesInfo>> {
@@ -38,11 +42,21 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
 
         AbsListView listView = (AbsListView) findViewById(R.id.list);
+
+        // Add header depending on device orientation
+        int orientation = getResources().getConfiguration().orientation;
+        if (orientation == ORIENTATION_PORTRAIT) {
+            View view = getLayoutInflater().inflate(R.layout.listview_header, null);
+            ((ListView) listView).addHeaderView(view);
+        }
+
+        // Create the adapter and set it to the listview
         quotesAdapter = new QuotesAdapter(this,
                                           R.layout.quotes_list_item,
                                           new ArrayList<QuotesInfo>());
         listView.setAdapter(quotesAdapter);
 
+        // Start the loader
         getSupportLoaderManager().initLoader(LOADER_ID, null, this).forceLoad();
     }
 }
