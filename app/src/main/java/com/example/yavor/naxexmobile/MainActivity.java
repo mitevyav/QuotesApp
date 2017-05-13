f8cbcf636100ac9d2ecc4be6881ab4446e3d214e
ymitev <yavor.mitev@securax.org>
2017-05-13 13:28:50 +0300

    Use fragment dialog instead plain one.

--- app/src/main/java/com/example/yavor/naxexmobile/MainActivity.java
+++ app/src/main/java/com/example/yavor/naxexmobile/MainActivity.java
@@ -1,25 +1,18 @@
 package com.example.yavor.naxexmobile;
 
-import android.app.Dialog;
-import android.content.DialogInterface;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.util.SparseBooleanArray;
 import android.view.View;
 import android.widget.AbsListView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static android.R.layout.simple_list_item_multiple_choice;
 import static android.content.res.Configuration.ORIENTATION_PORTRAIT;
 
 public class MainActivity extends AppCompatActivity
@@ -27,7 +20,7 @@ public class MainActivity extends AppCompatActivity
 
     private static final String LOG_TAG = MainActivity.class.getCanonicalName();
 
-    private static final int LOADER_ID = 1;
+    public static final int LOADER_ID = 1;
 
     private QuotesAdapter quotesAdapter;
 
@@ -90,96 +83,9 @@ public class MainActivity extends AppCompatActivity
      * Show a dialog with where the user can select the symbols to be shown.
      */
     private void showAddRemoveDialog() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle(getString(R.string.symbols_dialog_title));
-
-        // Create the ListView and set the adapter
-        final ListView listView = new ListView(this);
-        listView.setChoiceMode(AbsListView.CHOICE_MODE_MULTIPLE);
-        final String[] stringArray = Utils.getSymbols(getResources());
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
-                                                          simple_list_item_multiple_choice,
-                                                          android.R.id.text1,
-                                                          stringArray);
-        listView.setAdapter(adapter);
-        builder.setView(listView);
-
-        loadCheckedPositions(listView, stringArray);
-
-        builder.setPositiveButton(getString(android.R.string.ok),
-                                  new DialogInterface.OnClickListener() {
-                                      @Override
-                                      public void onClick(DialogInterface dialogInterface, int i) {
-                                          String query =
-                                                  getSymbolsQueryValue(listView.getCheckedItemPositions(),
-                                                                       stringArray);
-                                          saveQuery(query);
-                                          getSupportLoaderManager().getLoader(LOADER_ID)
-                                                                   .forceLoad();
-                                          dialogInterface.dismiss();
-                                      }
-                                  });
-
-        builder.setNegativeButton(getString(android.R.string.cancel),
-                                  new DialogInterface.OnClickListener() {
-                                      @Override
-                                      public void onClick(DialogInterface dialogInterface, int i) {
-                                          dialogInterface.dismiss();
-                                      }
-                                  });
-        final Dialog dialog = builder.create();
-
-        dialog.show();
+        FragmentManager fm = getSupportFragmentManager();
+        SymbolsDialog dialogFragment = new SymbolsDialog();
+        dialogFragment.show(fm, "");
     }
 
-    /**
-     * Create the symbols query depending on the checked positions
-     *
-     * @param checkedItemPositions
-     *         the positions checked
-     * @param stringArray
-     */
-    private String getSymbolsQueryValue(SparseBooleanArray checkedItemPositions,
-                                        String[] stringArray) {
-        StringBuilder builder = new StringBuilder();
-        for (int i = 0; i < stringArray.length; i++) {
-            if (checkedItemPositions.get(i)) {
-                builder.append(stringArray[i] + ",");
-            }
-        }
-        // Remove the last ","
-        if (builder.length() > 0) {
-            builder.replace(builder.length(), builder.length(), "");
-        }
-        return builder.toString();
-    }
-
-    /**
-     * Save the quotes query in a pref field.
-     *
-     * @param query
-     */
-    private void saveQuery(String query) {
-        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
-        SharedPreferences.Editor editor = preferences.edit();
-        editor.putString(getString(R.string.symbols_pref_key), query);
-        editor.commit();
-    }
-
-    /**
-     * Set the list items checked depending on the current pref query
-     *
-     * @param listView listView showing the items
-     * @param stringArray array with all possible symbol query params.  
-     */
-    private void loadCheckedPositions(ListView listView, String[] stringArray) {
-        String query = Utils.getSymbolsQuery(this);
-
-        for (int i = 0; i < stringArray.length; i++) {
-            int result = query.indexOf(stringArray[i]);
-            if (result >= 0) {
-                listView.setItemChecked(i, true);
-            }
-        }
-    }
 }
---
+++ app/src/main/java/com/example/yavor/naxexmobile/SymbolsDialog.java
@@ -0,0 +1,121 @@
+package com.example.yavor.naxexmobile;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.util.SparseBooleanArray;
+import android.widget.AbsListView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import static android.R.layout.simple_list_item_multiple_choice;
+import static com.example.yavor.naxexmobile.MainActivity.LOADER_ID;
+
+/**
+ * Created by mitevyav on 13.5.2017 г..
+ */
+
+public class SymbolsDialog extends DialogFragment {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle(getString(R.string.symbols_dialog_title));
+
+        // Create the ListView and set the adapter
+        final ListView listView = new ListView(getActivity());
+        listView.setChoiceMode(AbsListView.CHOICE_MODE_MULTIPLE);
+        final String[] stringArray = Utils.getSymbols(getResources());
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(),
+                                                          simple_list_item_multiple_choice,
+                                                          android.R.id.text1,
+                                                          stringArray);
+        listView.setAdapter(adapter);
+        builder.setView(listView);
+
+        loadCheckedPositions(listView, stringArray);
+
+        builder.setPositiveButton(getString(android.R.string.ok),
+                                  new DialogInterface.OnClickListener() {
+                                      @Override
+                                      public void onClick(DialogInterface dialogInterface, int i) {
+                                          String query =
+                                                  getSymbolsQueryValue(listView.getCheckedItemPositions(),
+                                                                       stringArray);
+                                          saveQuery(query);
+                                          getActivity().getSupportLoaderManager().getLoader(
+                                                  LOADER_ID).forceLoad();
+                                          dialogInterface.dismiss();
+                                      }
+                                  });
+
+        builder.setNegativeButton(getString(android.R.string.cancel),
+                                  new DialogInterface.OnClickListener() {
+                                      @Override
+                                      public void onClick(DialogInterface dialogInterface, int i) {
+                                          dialogInterface.dismiss();
+                                      }
+                                  });
+        return builder.create();
+    }
+
+    /**
+     * Create the symbols query depending on the checked positions
+     *
+     * @param checkedItemPositions
+     *         the positions checked
+     * @param stringArray
+     */
+    private String getSymbolsQueryValue(SparseBooleanArray checkedItemPositions,
+                                        String[] stringArray) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < stringArray.length; i++) {
+            if (checkedItemPositions.get(i)) {
+                builder.append(stringArray[i] + ",");
+            }
+        }
+        // Remove the last ","
+        if (builder.length() > 0) {
+            builder.replace(builder.length(), builder.length(), "");
+        }
+        return builder.toString();
+    }
+
+    /**
+     * Save the quotes query in a pref field.
+     *
+     * @param query
+     */
+    private void saveQuery(String query) {
+        SharedPreferences preferences =
+                PreferenceManager.getDefaultSharedPreferences(getActivity());
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString(getString(R.string.symbols_pref_key), query);
+        editor.commit();
+    }
+
+    /**
+     * Set the list items checked depending on the current pref query
+     *
+     * @param listView
+     *         listView showing the items
+     * @param stringArray
+     *         array with all possible symbol query params.
+     */
+    private void loadCheckedPositions(ListView listView, String[] stringArray) {
+        String query = Utils.getSymbolsQuery(getActivity());
+
+        for (int i = 0; i < stringArray.length; i++) {
+            int result = query.indexOf(stringArray[i]);
+            if (result >= 0) {
+                listView.setItemChecked(i, true);
+            }
+        }
+    }
+}
